d5816e6e187a21dffe4b2f3301d8b2eb04fd772b
Strojki <estrojil1@etf.unsa.ba>
2015-05-04 23:01:36 +0200

    Treca spirala ispravka greski 2

--- JavaScript/formResetItem.js
+++ JavaScript/formResetItem.js
@@ -1,45 +1,30 @@
 /**
  * Created by Edin on 5/4/2015.
  */
-/*
-function formReset(){
+
+function formResetItem(){
 
     var naziv = document.getElementById('input_item_name');
-    var document.getElementById('input_item_description');
-    var document.getElementById('input_image');
-    var mail = document.getElementById('input_email');
-    var message_error = document.getElementById('message_error');
-    var message = document.getElementById('message_text_area');
-    var zipcode_error = document.getElementById('zipcode_error');
-    var zipcode = document.getElementById('input_zipcode');
-    naziv: ,
-        opis :,
-        slika:.value,
-        url: document.getElementById('input_URL').value,
-        dostupnost: (document.getElementById('input_availability').checked ? 1:0),
-        kolicina: document.getElementById('input_quantity').value,
-        cijena: parseFloat(document.getElementById('input_price').value)
+    var opis=document.getElementById('input_item_description');
+    var slika=document.getElementById('input_image');
+    var url= document.getElementById('input_URL');
+    var dostupnost =message_error = document.getElementById('input_availability');
+    var kolicina= message = document.getElementById('input_quantity');
+    var cijena = document.getElementById('input_price');
 
-    name_error.style.display = "none";
-    name.style.backgroundColor ="white";
-    mail_error.style.display = "none";
-    mail.style.backgroundColor ="white";
-    message_error.style.display ="none";
-    message.style.backgroundColor ="white";
-    zipcode_error.style.display ="none";
-    zipcode.style.backgroundColor ="white";
 
-    document.getElementById('unchecked').style.display= "none";
-    document.getElementById('checked').style.display= "none";
-    document.getElementById('unchecked2').style.display= "none";
-    document.getElementById('checked2').style.display= "none";
-    document.getElementById('unchecked3').style.display= "none";
-    document.getElementById('checked3').style.display= "none";
-    document.getElementById('unchecked4').style.display= "none";
-    document.getElementById('checked4').style.display= "none";
+    naziv.style.backgroundColor ="white";
+
+    opis.style.backgroundColor ="white";
+
+    slika.style.backgroundColor ="white";
+
+    url.style.backgroundColor ="white";
+    dostupnost.style.backgroundColor ="white";
+    kolicina.style.backgroundColor ="white";
+    cijena.style.backgroundColor ="white";
 
 
 
 
-
-}*/
+}
